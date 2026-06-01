1b8221f6b2fc2b3432e1f68986587bf53381efad
Randriamanampisoa <fanilonomenjanahary062@gmail.com>
2026-06-01 12:52:44 +0300

    feat: test for ft_value_security.py

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -7,7 +7,14 @@
 #   By: fanilran <fanilran@student.42.fr>            +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/28 13:56:13 by fanilran            #+#    #+#            #
-#   Updated: 2026/05/28 13:56:14 by fanilran           ###   ########.fr      #
+#   Updated: 2026/06/01 12:40:01 by fanilran           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
+def secure_archive(file_name: str):
+    print("=== Cyber Archives Security ===\n")
+    print(file_name)
+
+
+if __name__ == "__main__":
+    secure_archive("fanilo")
