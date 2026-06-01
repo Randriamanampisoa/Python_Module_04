c7ff19025c7009db0755bc5908236409a317ed14
Randriamanampisoa <fanilonomenjanahary062@gmail.com>
2026-06-01 12:51:13 +0300

    feat: update for the name variable

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -7,7 +7,7 @@
 #   By: fanilran <fanilran@student.42.fr>            +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/29 23:04:29 by fanilran            #+#    #+#            #
-#   Updated: 2026/05/29 23:33:16 by fanilran           ###   ########.fr      #
+#   Updated: 2026/06/01 12:48:25 by fanilran           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -42,14 +42,14 @@ if __name__ == "__main__":
                 print("Not saving data.")
             else:
                 print(f"Saving data to '{new_file}'")
-                file_magic = None
+                new_file_up = None
                 try:
-                    file_magic = open(new_file, "w")
-                    file_magic.write("\n".join(new_content))
+                    new_file_up = open(new_file, "w")
+                    new_file_up.write("\n".join(new_content))
                     print(f"Data saved in file '{new_file}'.")
                 finally:
-                    if file_magic:
-                        file_magic.close()
+                    if new_file_up:
+                        new_file_up.close()
         except Exception as e:
             print(f"Error opening file '{sys.argv[1]}': {e}")
         finally:
